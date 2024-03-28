d6be6f8f305d90cad069b7b33b1bf0f50ffb9f84
Marc Gravell <marc.gravell@gmail.com>
2024-03-28 12:08:24 +0000

    spike towards in-place deserialize

--- src/FASTERCache/FASTERCacheServiceExtensions.cs
+++ src/FASTERCache/FASTERCacheServiceExtensions.cs
@@ -2,6 +2,9 @@
 using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using System;
+using System.Buffers;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Microsoft.Extensions.DependencyInjection;
 
@@ -10,6 +13,55 @@ namespace Microsoft.Extensions.DependencyInjection;
 /// </summary>
 public static class FASTERCacheServiceExtensions
 {
+    public static TValue Get<TState, TValue>(this IDistributedCache cache, string key, in TState state, Deserializer<TState, TValue> deserializer)
+    {
+        if (cache is IExperimentalBufferCache cacheEx)
+        {
+            return cacheEx.Get(key, state, deserializer);
+        }
+        var arr = cache.Get(key);
+        return deserializer(state, arr is not null, arr);
+    }
+
+    internal static TValue Get<TState, TValue>(this IExperimentalBufferCache cache, string key, in TState state, Deserializer<TState, TValue> deserializer)
+    {
+        var bw = new ArrayBufferWriter<byte>(); // TODO: recycling
+        return deserializer(state, cache.Get(key, bw), bw.WrittenSpan);
+    }
+
+    public static ValueTask<TValue> GetAsync<TState, TValue>(this IDistributedCache cache, string key, in TState state, Deserializer<TState, TValue> deserializer, CancellationToken token = default)
+    {
+        if (cache is IExperimentalBufferCache cacheEx)
+        {
+            return cacheEx.GetAsync(key, state, deserializer, token);
+        }
+        var pending = cache.GetAsync(key, token);
+        if (!pending.IsCompletedSuccessfully) return Awaited(pending, state, deserializer);
+
+        var arr = pending.GetAwaiter().GetResult();
+        return new(deserializer(state, arr is not null, arr));
+
+        static async ValueTask<TValue> Awaited(Task<byte[]?> pending, TState state, Deserializer<TState, TValue> deserializer)
+        {
+            var arr = await pending;
+            return deserializer(state, arr is not null, arr);
+        }
+    }
+
+    internal static ValueTask<TValue> GetAsync<TState, TValue>(this IExperimentalBufferCache cache, string key, in TState state, Deserializer<TState, TValue> deserializer, CancellationToken token = default)
+    {
+        var bw = new ArrayBufferWriter<byte>(); // TODO: recycling
+        var pending = cache.GetAsync(key, bw, token);
+        if (!pending.IsCompletedSuccessfully) return Awaited(pending, bw, state, deserializer);
+
+        return new(deserializer(state, pending.GetAwaiter().GetResult(), bw.WrittenSpan));
+
+        static async ValueTask<TValue> Awaited(ValueTask<bool> pending, ArrayBufferWriter<byte> bw, TState state, Deserializer<TState, TValue> deserializer)
+        {
+            return deserializer(state, await pending, bw.WrittenSpan);
+        }
+    }
+
     public static void AddFASTERCache(this IServiceCollection services, Action<FASTERCacheOptions> setupAction)
     {
         ArgumentNullException.ThrowIfNull(services);
---
+++ src/FASTERCache/FASTERDeserializer.cs
@@ -0,0 +1,4 @@
+﻿using System;
+
+namespace FASTERCache;
+public delegate TValue Deserializer<TState, TValue>(in TState state, bool found, ReadOnlySpan<byte> payload);
